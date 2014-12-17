eb759da79bc262d4e3510a95784e968b4d092e1c
Calvin Leung Huang <cleung@cimpress.com>
2014-12-17 11:06:58 -0500

    Changed download url to point correctly

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,16 @@
 class consul_replicate::params {
   $version = '0.1.0'
 
+  case $::architecture {
+    'x86_64', 'amd64': { $arch = 'amd64' }
+    'i386':            { $arch = '386'   }
+    default:           { fail("Unsupported kernel architecture: ${::architecture}") }
+  }
+
   case $::operatingsystem {
     ubuntu: {
       $bin_dir      = '/usr/local/bin'
-      $download_url = "https://github.com/hashicorp/consul-replicate/releases/download/v${version}/consul-replicate_linux_${::operatingsystem}" 
+      $download_url = "https://github.com/hashicorp/consul-replicate/releases/download/v${version}/consul-replicate_linux_${arch}" 
     }
     default: { fail("Unsupported operating system: ${::operatingsystem}") }
   }
