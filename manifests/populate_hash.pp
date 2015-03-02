f1677ba801b7cf4f48b285630dc9dd09d3a05b1b
Calvin Leung Huang <cleung@cimpress.com>
2015-03-02 13:48:26 -0500

    Add new files

---
+++ manifests/populate_hash.pp
@@ -0,0 +1,4 @@
+# See README.md for details
+class consul_replicate::populate_hash {
+  $merged_config_hash = merge(hiera_hash('consul_replicate::hiera_config_hash', {}), $consul_replicate::config_hash)
+}
