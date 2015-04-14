8b9a5592fbb72750f2f0f48b1a33d9c36f3dbc0f
Calvin Leung Huang <cleung2010@gmail.com>
2015-04-14 15:53:09 -0400

    Make sure sensu package is installed first

--- manifests/monitoring/sensu.pp
+++ manifests/monitoring/sensu.pp
@@ -3,12 +3,12 @@ class consul_replicate::monitoring::sensu {
 
   package { 'sensu':
     ensure => installed
-  }
+  } ->
 
   file { 'Ensure check-procs.rb is present':
     ensure => file,
     path   => '/etc/sensu/plugins/processes/check-procs.rb'
-  }
+  } ->
 
   file { '/etc/sensu/conf.d/consul_replicate_config.json':
     ensure  => file,
